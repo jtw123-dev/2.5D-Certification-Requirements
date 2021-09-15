191acde392829def70886b9ae5119de02921f808
jtw123-dev <78871610+jtw123-dev@users.noreply.github.com>
2021-09-15 09:58:45 -0400

    Added modular platform

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/MoveablePlatform.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveablePlatform : MonoBehaviour
+{
+    [SerializeField] Transform _pointA, _pointB;
+    [SerializeField]private bool _switch;
+    private float _speed = 5;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="PointB")
+        {
+            _switch = true;
+        }
+        else if (other.tag=="PointA")
+        {
+            _switch = false;
+        }
+    }
+    private void Update()
+    {
+        if (_switch == false)
+        {
+            transform.Translate(Vector3.left * _speed * Time.deltaTime);
+        }
+        if (_switch==true)
+        {
+            transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        }
+    }
+}
