97d82d82faf76522b66226ef88e30909d5184e3e
jtw123-dev <78871610+jtw123-dev@users.noreply.github.com>
2021-09-16 15:24:50 -0400

    Fixed movable platform

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/MoveablePlatform.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/MoveablePlatform.cs
@@ -6,33 +6,35 @@ public class MoveablePlatform : MonoBehaviour
 {
     [SerializeField] Transform _pointA, _pointB;
     [SerializeField]private bool _switch;
-    private float _speed = 5;
+    [SerializeField]private float _speed = 5;
 
+   
+    private void Update()
+    {
+       if (_switch==false)
+        {
+            transform.Translate(Vector3.back * _speed * Time.deltaTime);
+        }
+            
+        if (_switch==true)
+        {
+            transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
+        if (other.tag=="PointA")
+        {
+            _switch = false;
+        }
         if (other.tag=="PointB")
         {
             _switch = true;
         }
-        else if (other.tag=="PointA")
+        if (other.tag == "Player")
         {
-            _switch = false;
-        }
-        if (other.tag=="Player")
-        {
-            Debug.Log("moving platform");
-            other.transform.parent = gameObject.transform.parent;
-        }
-    }
-    private void Update()
-    {
-        if (_switch == false)
-        {
-            transform.Translate(Vector3.left * _speed * Time.deltaTime);
-        }
-        if (_switch==true)
-        {
-            transform.Translate(Vector3.right * _speed * Time.deltaTime);
+            Debug.Log("Player was here");
+            other.transform.parent = gameObject.transform;
         }
     }
 }
